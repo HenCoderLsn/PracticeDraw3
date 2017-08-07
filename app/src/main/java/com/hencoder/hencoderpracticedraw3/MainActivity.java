7549747c08918b5e423bc0921a3728190a0544e1
XieHui <842205017@qq.com>
2017-08-07 17:14:47 +0800

    Update MainActivity.java
    test

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/MainActivity.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    
     TabLayout tabLayout;
     ViewPager pager;
     List<PageModel> pageModels = new ArrayList<>();
